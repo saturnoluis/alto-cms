7c051d05b35de38deb59651d8c3d8f4e9caa86af
Luis Saturno <saturno.luis@gmail.com>
2023-03-10 17:43:37 -0300

    Add react router
    Run npm install react-router-dom
    Create basic routes inside App.js

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,10 +1,20 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
     return (
-        <div className="App">
-            <p>Hello world</p>
-        </div>
+        <BrowserRouter>
+            <div className="App">
+                <Routes>
+                    <Route exact path="/" element={<p>Home</p>} />
+                    <Route exact path="/about" element={<p>About</p>} />
+                    <Route exact path="/blog" element={<p>Blog</p>} />
+                    <Route exact path="/contact" element={<p>Contact</p>} />
+                </Routes>
+            </div>
+        </BrowserRouter>
     );
 }
 
